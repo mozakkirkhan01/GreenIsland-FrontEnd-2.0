29b1a0aae48c8113e026ce675c91696ed7e7e4e0
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2026-03-03 23:45:17 +0530

    feat: update app configuration and routing
    - Enhanced app configuration in `app.config.ts` by adding providers for HTTP client, animations, and Toastr notifications.
    - Updated routing in `app.routes.ts` to include admin login and dashboard routes, with a redirect for unmatched paths.
    - Removed placeholder content from `app.html` and added a router outlet for navigation.
    - Adjusted CSS styles in `app.min.css` and `bootstrap.min.css` for better layout and breadcrumb display.
    - Integrated jQuery into the application by exposing it to the global window object in `main.ts`.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { AdminDashboard } from './admin/admin-dashboard/admin-dashboard';
+import { AdminMaster } from './admin/admin-master/admin-master';
+import { AdminLogin } from './admin/admin-login/admin-login';
+import { Company } from './admin/company/company';
+export const routes: Routes = [
+// 👉 First Page = Login
+  { path: '', redirectTo: 'admin-login', pathMatch: 'full' },
 
-export const routes: Routes = [];
+  { path: 'admin-login', component: AdminLogin },
+
+  // 👉 Protected Area
+  {
+    path: 'admin',
+    component: AdminMaster,
+    children: [
+      { path: '', redirectTo: 'admin-dashboard', pathMatch: 'full' },
+      { path: 'admin-dashboard', component: AdminDashboard },
+      { path: 'company', component: Company},
+    //   { path: 'designation', component: DesignationComponent },
+    //   { path: 'department', component: DepartmentComponent },
+    //   { path: 'staff', component: StaffComponent },
+    //   { path: 'company', component: CompanyComponent }
+    ]
+  },
+
+  { path: '**', redirectTo: 'admin-login' }
+];
