05bfe15dcd77f8a1e5a01a7016409ad12087c387
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2026-03-04 22:18:47 +0530

    refactor: reorganize app configuration and enhance provider imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,50 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr, ToastrModule } from 'ngx-toastr';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+
+// import {
+//   NgxMatDatetimePickerModule,
+//   NgxMatNativeDateModule,
+//   NgxMatTimepickerModule
+// } from '@angular-material-components/datetime-picker';
+
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideToastr(),
+
+    importProvidersFrom(
+      FormsModule,
+      ReactiveFormsModule,
+     // ToastrModule.forRoot(),
+      NgxPaginationModule,
+      MatDatepickerModule,
+      MatInputModule,
+      MatFormFieldModule,
+      MatNativeDateModule
+      // NgxMatDatetimePickerModule,
+      // NgxMatNativeDateModule,
+      // NgxMatTimepickerModule
+    ),
+
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
+  
 };
