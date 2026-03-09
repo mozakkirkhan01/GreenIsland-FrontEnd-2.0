1ed80f54476739504d3e078d7584597c7b28e0c2
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2026-03-09 12:02:50 +0530

    add state management component with HTML, CSS, and unit tests

---
+++ src/app/admin/state/state.ts
@@ -0,0 +1,194 @@
+import { Component, ViewChild, ChangeDetectorRef, inject } from '@angular/core';
+import { NgForm, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { AppService } from '../../utils/app.service';
+import { ConstantData } from '../../utils/constant-data';
+import { LocalService } from '../../utils/local.service';
+import { LoadDataService } from '../../utils/load-data.service';
+import { Status } from '../../utils/enum';
+import { ActionModel, RequestModel, StaffLoginModel } from '../../utils/interface';
+import { FilterPipe } from '../../utils/filter-pipe';
+import { OrderByPipe } from '../../utils/orderby-pipe';
+
+@Component({
+  selector: 'app-state',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    NgxPaginationModule,
+    FilterPipe,
+    OrderByPipe,
+  ],
+  templateUrl: './state.html',
+  styleUrls: ['./state.css']
+})
+export class State {
+  dataLoading: boolean = false;
+  StateList: any = [];
+  State: any = {};
+  isSubmitted = false;
+  loadData = inject(LoadDataService);
+  StatusList = this.loadData.GetEnumList(Status);
+  AllStatusList = Status;
+  PageSize = ConstantData.PageSizes;
+  p: number = 1;
+  Search: string = '';
+  reverse: boolean = false;
+  sortKey: string = '';
+  itemPerPage: number = this.PageSize[0];
+  action: ActionModel = {} as ActionModel;
+  staffLogin: StaffLoginModel = {} as StaffLoginModel;
+  showModal: boolean = false;
+
+  sort(key: any) {
+    this.sortKey = key;
+    this.reverse = !this.reverse;
+  }
+
+  onTableDataChange(p: any) {
+    this.p = p;
+  }
+
+  constructor(
+    private service: AppService,
+    private toastr: ToastrService,
+    private localService: LocalService,
+    private router: Router,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.staffLogin = this.localService.getEmployeeDetail();
+    this.validiateMenu();
+    this.getStateList();
+    this.resetForm();
+  }
+
+  validiateMenu() {
+    var obj: RequestModel = {
+      request: this.localService.encrypt(
+        JSON.stringify({ Url: this.router.url, StaffLoginId: this.staffLogin.StaffLoginId })
+      ).toString()
+    };
+    this.dataLoading = true;
+    this.service.validiateMenu(obj).subscribe((response: any) => {
+      this.action = { ...this.loadData.validiateMenu(response, this.toastr, this.router) };
+      this.dataLoading = false;
+      this.cdr.detectChanges();
+    }, err => {
+      this.toastr.error("Error while fetching records");
+      this.dataLoading = false;
+    });
+  }
+
+  @ViewChild('formState') formState!: NgForm;
+
+  resetForm() {
+    this.State = { Status: 1 };
+    if (this.formState) {
+      this.formState.control.markAsPristine();
+      this.formState.control.markAsUntouched();
+    }
+    this.isSubmitted = false;
+  }
+
+  openModal() {
+    this.resetForm();
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.resetForm();
+    this.showModal = false;
+  }
+
+  getStateList() {
+    var obj: RequestModel = {
+      request: this.localService.encrypt(JSON.stringify({})).toString()
+    };
+    this.dataLoading = true;
+    this.service.getStateList(obj).subscribe(r1 => {
+      let response = r1 as any;
+      if (response.Message == ConstantData.SuccessMessage) {
+        this.StateList = response.StateList;
+      } else {
+        this.toastr.error(response.Message);
+      }
+      this.dataLoading = false;
+    }, err => {
+      this.toastr.error("Error while fetching records");
+      this.dataLoading = false;
+    });
+  }
+
+  saveState() {
+    this.isSubmitted = true;
+    this.formState.control.markAllAsTouched();
+    if (this.formState.invalid) {
+      this.toastr.error("Fill all the required fields !!");
+      return;
+    }
+    this.State.CreatedBy = this.staffLogin.StaffLoginId;
+    this.State.UpdatedBy = this.staffLogin.StaffLoginId;
+    var obj: RequestModel = {
+      request: this.localService.encrypt(JSON.stringify(this.State)).toString()
+    };
+    this.dataLoading = true;
+    this.service.saveState(obj).subscribe(r1 => {
+      let response = r1 as any;
+      if (response.Message == ConstantData.SuccessMessage) {
+        this.toastr.success(this.State.StateId > 0
+          ? "State updated successfully"
+          : "State added successfully");
+        this.closeModal();
+        this.getStateList();
+      } else {
+        this.toastr.error(response.Message);
+        this.dataLoading = false;
+      }
+    }, err => {
+      this.toastr.error("Error occured while submitting data");
+      this.dataLoading = false;
+    });
+  }
+
+  deleteState(obj: any) {
+    if (confirm("Are you sure you want to delete this record?")) {
+      var request: RequestModel = {
+        request: this.localService.encrypt(JSON.stringify(obj)).toString()
+      };
+      this.dataLoading = true;
+      this.service.deleteState(request).subscribe(r1 => {
+        let response = r1 as any;
+        if (response.Message == ConstantData.SuccessMessage) {
+          this.toastr.success("Record deleted successfully");
+          this.getStateList();
+        } else {
+          this.toastr.error(response.Message);
+          this.dataLoading = false;
+        }
+      }, err => {
+        this.toastr.error("Error occured while deleting the record");
+        this.dataLoading = false;
+      });
+    }
+  }
+
+  editState(obj: any) {
+    this.State = { ...obj };
+    this.showModal = true;
+  }
+}
